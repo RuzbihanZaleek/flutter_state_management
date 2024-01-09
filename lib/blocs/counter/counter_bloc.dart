9f757dcc613a7d8373aeb396687c45e3983ab0b4
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2024-01-09 22:46:24 +0530

    1. bloc package

---
+++ lib/blocs/counter/counter_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_event.dart';
+
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterInitial()) {
+    on<CounterEvent>((event, emit) {
+      int value;
+      if (event is IncrementEvent) {
+        value = event.value;
+        value++;
+        emit(IncrementState(value: value));
+      } else if (event is DecrementEvent) {
+        value = event.value;
+        value--;
+        emit(DecrementState(value: value));
+      }
+    });
+  }
+}
